e45c6bced5cb5630c8abb194b32daced70df31a7
Nicholas BURNESS <Nicholas.BURNESS@EDUCATION.GOV.UK>
2024-12-31 13:01:56 +0000

    use ejs view engine and scaffold the home and charts pages to render basic data fetched by the currencyService in a html table
    create seperate stylesheet

--- src/app.js
+++ src/app.js
@@ -4,30 +4,50 @@ import CurrencyRepository from './repositories/currencyRepository.js';
 import CurrencyService from './services/currencyService.js';
 
 const PORT = config.port
-const app = express()
+const app = express() 
+
+app.set('view engine', 'ejs') // Define ejs as the view engine
+app.set('views', './views') // Set directory where view files are 
 
 // Initialise the currency service and repository
-const currencyService = new CurrencyService();
-const currencyRepo = new CurrencyRepository(currencyService);
+const currencyService = new CurrencyService()
+const currencyRepo = new CurrencyRepository(currencyService)
 
 // Home route
 app.get('/', async (req, res) => {
   try {
-    // Initialize the repository which fetches data from the CurrencyService
+    // Initialise the repository which fetches data from the CurrencyService
     await currencyRepo.initialize();
-
-    // Example usage: Get all currencies
+    
+    // get all available currencies
     const currencies = currencyRepo.findAll();
 
     // Log the currencies to verify
     console.log('Currencies:', currencies);
 
-    // Send a response back to the client (you could also render data in a template or send JSON)
-    res.json(currencies);  // Sending the list of currencies back as JSON
+    // Render EJS template and pass the currencies data with the request
+    res.render('home', { currencies });
     
   } catch (error) {
-    console.error('Error during API request or repository initialization:', error);
+    console.error('Error during API request or repository initialisation:', error);
     res.status(500).send('Error occurred during API request');
+  }
+});
+
+// Charts route 
+app.get('/charts', async (req, res) => {
+  try {
+    // to be implemented
+    const currencies = currencyRepo.findAll();
+
+    // Log the currencies to verify
+    console.log('Currencies:', currencies);
+
+    // render EJS template and pass the currencies data with the request
+    res.render('charts', { currencies});
+
+  } catch (error) {
+    console.error("Error during API request of repository initialisation", error)
   }
 });
 
