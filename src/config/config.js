d74201e45d2da6992273103837259560fdbfec61
Nicholas BURNESS <Nicholas.BURNESS@EDUCATION.GOV.UK>
2025-01-10 10:19:33 +0000

    Add a development, test and production environment specific configuration file and algorithm for node to select the active environments config file to use

--- src/config/config.js
+++ src/config/config.js
@@ -1,6 +1,29 @@
 import dotenv from 'dotenv'
 
-dotenv.config();
+let environmentFile;
+
+console.log(`Current NODE_ENV: ${process.env.NODE_ENV}`)
+
+// determine which environment config file to load
+switch(process.env.NODE_ENV) {
+    case 'development':
+        environmentFile = '.env.development'
+        break
+    case 'test':
+        environmentFile = '.env.test'
+        break
+    case 'production':
+        environmentFile = '.env.production'
+        break
+    // if NODE_ENV is undefined, indicating local development use the basic .env files
+    default:
+        environmentFile = '.env'
+        break
+}
+
+// load the selected .env file
+dotenv.config({path: environmentFile});
+console.log(`Loaded environment variables from: ${environmentFile}`)
 
 export default {
     apiKey: process.env.API_KEY,
